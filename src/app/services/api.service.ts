6f4f28ec331a5ef372abdee4f145f6cfbb7eeefc
Huiren Woo <giantcrabby@gmail.com>
2021-03-23 08:30:32 +0800

    Add api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEndpointHostUrl(): string {
+    return environment.endpoint;
+  }
+
+  public get(path: string, data: { [param: string]: string }): Observable<any> {
+    let params = new HttpParams();
+    for (const dataKey in data) {
+      if (dataKey && data[dataKey]) {
+        params = params.set(dataKey, data[dataKey]);
+      }
+    }
+    const options = {
+      headers: this.getJsonHeaders(),
+      params,
+    };
+    return this.http.get<any>(this.getEndpointHostUrl() + path, options);
+  }
+
+  private getJsonHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    });
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'https://explorer.ldoge-wow.com'
 };
