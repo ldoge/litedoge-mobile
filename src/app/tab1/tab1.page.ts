5ab4f829e2553912e9a3dc0fecf4c60b45517861
Huiren Woo <giantcrabby@gmail.com>
2021-04-02 02:03:52 +0800

    Change tab1 to onPush change detection

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,12 @@
-import {Component} from '@angular/core';
-import {SingleWallet} from '../models/single-wallet';
-import {SingleWalletGeneratorService} from '../services/single-wallet-generator.service';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {JaninService} from '../services/janin.service';
 import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Tab1Page {
   public showPrivateKey$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
