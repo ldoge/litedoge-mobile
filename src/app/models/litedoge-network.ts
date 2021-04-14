ce0bf555a01fafa59a29cdd4f8618fcfa99afd7c
Huiren Woo <giantcrabby@gmail.com>
2021-04-14 14:00:17 +0800

    Change to readonly

--- src/app/models/litedoge-network.ts
+++ src/app/models/litedoge-network.ts
@@ -1,19 +1,19 @@
 import {Network} from 'bitcoinjs-lib';
 
 export class LitedogeNetwork implements Network {
-  public messagePrefix = '\x18LiteDoge Signed Message:\n';
+  public readonly messagePrefix = '\x18LiteDoge Signed Message:\n';
   // LiteDoge doesn't seem to have bech32 yet
-  public bech32 = null;
-  public bip32 = {
+  public readonly bech32 = null;
+  public readonly bip32 = {
     // data from EXT_PUBLIC_KEY
     public: 0x0488B21E,
     // data from EXT_SECRET_KEY
     private: 0x0488ADE4,
   };
   // data from PUBKEY_ADDRESS
-  public pubKeyHash = 0x5a;
+  public readonly pubKeyHash = 0x5a;
   // data from SCRIPT_ADDRESS
-  public scriptHash = 0x07;
+  public readonly scriptHash = 0x07;
   // Seems like this should be correct
-  public wif = 0xab;
+  public readonly wif = 0xab;
 }
