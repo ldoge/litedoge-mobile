344c63e8115ae5051372c882e74db99803ccae71
Huiren Woo <giantcrabby@gmail.com>
2021-04-22 17:54:13 +0800

    Change how body is hidden

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,5 @@
 import {EventEmitter, Inject, Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
-import {DOCUMENT} from '@angular/common';
 import {Platform} from '@ionic/angular';
 
 @Injectable({
@@ -10,7 +9,7 @@ export class AppService {
   isHideBackground$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private backButton$: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(@Inject(DOCUMENT) private document: Document, private platform: Platform) {
+  constructor(private platform: Platform) {
     this.platform
       .backButton
       .subscribeWithPriority(0, async () => {
@@ -20,12 +19,10 @@ export class AppService {
 
   public hideBackground() {
     this.isHideBackground$.next(true);
-    this.document.body.classList.add('transparent-body');
   }
 
   public showBackground() {
     this.isHideBackground$.next(false);
-    this.document.body.classList.remove('transparent-body');
   }
 
   public triggerBackButton(): void {
