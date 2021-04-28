09b3d3c3f2c1addc25379c4a0c5f3acc3b5fcbc3
Huiren Woo <giantcrabby@gmail.com>
2021-04-29 01:34:28 +0800

    Add i18n config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import {RouteReuseStrategy} from '@angular/router';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {Storage} from '@ionic/storage';
 import {IonicStorageModule} from '@ionic/storage-angular';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SharedComponentsModule} from './shared-components/shared-components.module';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +23,13 @@ import {SharedComponentsModule} from './shared-components/shared-components.modu
     BrowserModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     HttpClientModule,
     AppRoutingModule,
     SharedComponentsModule,
