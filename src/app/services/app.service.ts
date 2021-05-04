5bd04a99134aeb899bb7866b3fb7472c5c42430c
Huiren Woo <giantcrabby@gmail.com>
2021-05-05 05:01:30 +0800

    Add quiting of application on back

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,9 @@
-import {EventEmitter, Inject, Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
-import {Platform} from '@ionic/angular';
+import {IonRouterOutlet, Platform} from '@ionic/angular';
+import {Plugins} from '@capacitor/core';
+
+const {App} = Plugins;
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +12,19 @@ export class AppService {
   isHideBackground$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private backButton$: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform,
+              private routerOutlet: IonRouterOutlet) {
     this.platform
       .backButton
       .subscribeWithPriority(0, async () => {
         this.backButton$.emit();
+
+        if (!this.isHideBackground$.getValue()) {
+          // Background not hidden
+          if (!this.routerOutlet.canGoBack()) {
+            App.exitApp();
+          }
+        }
       });
   }
 
